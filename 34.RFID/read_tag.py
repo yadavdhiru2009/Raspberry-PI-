4451478616d89e0d29eb52a8cad3fb2b3fe0229c
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-09-04 22:55:29 +0530

    RFID Interface With Raspberry Pi

---
+++ 34.RFID/read_tag.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python
+#Author: Dharmendra Kumar Yadav
+#######################################################
+
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
+reader = SimpleMFRC522()
+
+try:
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id,text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
