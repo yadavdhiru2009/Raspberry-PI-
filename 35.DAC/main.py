697344f1730edd43596114ff33461d139bc5c180
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-09-04 22:56:18 +0530

    DAC(Digital To Analog Converter) using Raspberry Pi

---
+++ 35.DAC/main.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python
+#Author: Dharmendra Kumar Yadav
+#######################################################
+
+import smbus
+import time
+
+address = 0x48
+cmd = 0x40
+value = 0
+
+bus = smbus.SMBus(1)
+while True:
+    bus.write_byte_data(address,cmd,value)
+    value += 1
+    if value == 256:
+        value =0
+    print("AOUT:%3d" %value)
+    time.sleep(0.01)
