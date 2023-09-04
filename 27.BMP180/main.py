4704285614bbf38e4392a58df5e038da81e60423
yadavdhiru2009 <yadavdhiru2009@gmail.com>
2023-09-04 22:50:19 +0530

    BMP180 Pressure sensor Interface With Raspberry Pi

---
+++ 27.BMP180/main.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python
+#Author: Dharmendra Kumar Yadav
+#######################################################
+
+import bmpsensor
+import time
+while True:
+    temp, pressure, altitude = bmpsensor.readBmp180()
+    print("Temperature is ",temp)  # degC
+    print("Pressure is ",pressure) # Pressure in Pa 
+    print("Altitude is ",altitude) # Altitude in meters
+    print("\n")
+    time.sleep(2)
